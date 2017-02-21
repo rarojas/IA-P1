5fa35a68a91f3c10ed5eb37e189bb2904711f086
Richard Parker <rora@gft.com>
2017-02-21 00:29:22 -0600

    select type

--- Cell.py
+++ Cell.py
@@ -1,4 +1,45 @@
-def Cell:
-     def __init__(self, x, y)
-        self.x = x
-        self.y = y
+class TypeCell:
+    MOUNTAIN,LAND, WATER, SAND,FORREST = range(5)
+    def __init__(self, Type):
+        self.value = Type
+
+    def __str__(self):
+        if self.value == TypeCell.MOUNTAIN:
+            return 'gray'
+        if self.value == TypeCell.LAND:
+            return 'brown'
+        if self.value == TypeCell.WATER:
+            return 'blue'
+        if self.value == TypeCell.SAND:
+            return 'yellow'
+        if self.value == TypeCell.FORREST:
+            return 'green'
+    @staticmethod
+    def parse(value):
+        if value == "MOUNTAIN":
+            return TypeCell.MOUNTAIN
+        if value == "LAND":
+            return TypeCell.LAND
+        if value == "WATER":
+            return TypeCell.WATER
+        if value == "SAND":
+            return TypeCell.SAND
+        if value == "FORREST":
+            return TypeCell.FORREST
+
+
+
+    def __eq__(self,y):
+       return self.value==y.value
+
+
+class Cell(object):
+    size = 50
+    celltype = TypeCell( TypeCell.MOUNTAIN)
+    visited = None
+    visisible = False
+    itemId = None
+
+    def __init__(self, i,j):
+        self.i = i
+        self.j = j
--- main.py
+++ main.py
@@ -1,27 +1,82 @@
 from Tkinter import *
 from sys import argv
-master = Tk()
-script, filename = argv
-dataFile = open(filename)
-with dataFile as stream:
-    content = stream.readlines()
+from Cell import *
 
-noRows = 0
-noColumns = 0
-Matrix = [[]]
-for line in content:
-    row = line.replace("\n").split(',')
-    for cell in row:
-        no
-        noColumns++
-    print row
-    noRows++
-
-w = Canvas(master, width=600, height=600)
-w.pack()
-w.create_rectangle(50, 25, 150, 75, fill="blue")
+Selected = None
+cursor = (0,0)
+ItemList = {}
+field = []
+canvas = None
 
 
+def key(event):
+    print "pressed", repr(event.char)
+    #if(event.char == '')
+
+def callback(event):
+    print "clicked at", event.x, event.y
+    canvas = event.widget
+    x = canvas.canvasx(event.x)
+    y = canvas.canvasy(event.y)
+    rect = canvas.find_closest(x, y)[0]
+
+    item = ItemList[rect]
+    if item != None:
+        global Selected
+        global Cursor
+        cell = field[item[0]][item[1]]
+        Selected = cell
+        Cursor = (cell.i,cell.j)
+        canvas.itemconfig(rect, outline="blue")
+
+def onOptionsMenuSelection(event):
+    if Selected != None:
+        global Selected
+        Selected.celltype = TypeCell(TypeCell.parse(event))
+        canvas.itemconfig(Selected.itemId, fill = Selected.celltype)
 
 
-mainloop()
+def main():
+    master = Tk()
+    script, filename = argv
+    dataFile = open(filename)
+    with dataFile as stream:
+        content = stream.readlines()
+    noRows = 0
+    noColumns = 0
+    size = 50
+    matrix = []
+    for line in content:
+        row = line.replace("\n","").split(',')
+        matrix.append(row)
+        noRows+= 1
+    global canvas
+    canvas = Canvas(master, width=600, height=600)
+    canvas.pack()
+
+    variable = StringVar(master)
+    optionsField = ["MOUNTAIN", "LAND", "WATER","SAND","FORREST"]
+    options = OptionMenu(master, variable, *(optionsField),command = onOptionsMenuSelection)
+    options.pack()
+    canvas.bind("<Key>", key)
+
+    i = 0
+    for row in matrix:
+        j = 0
+        field.append([])
+        for cell in row:
+            square = Cell(i,j)
+            idRectangle = "rec:" + str(i) + "," + str(j)
+            rectangle = canvas.create_rectangle(i * size , j * size, (1 + i) * size , (1 + j)* size, fill= square.celltype, tag = idRectangle)
+            square.itemId = rectangle
+            canvas.tag_bind(idRectangle, "<Button-1>", callback)
+            ItemList[rectangle] = (i,j)
+            field[i].append(square)
+            j += 1
+        i += 1
+
+
+    master.mainloop()
+
+if __name__ == "__main__":
+    main()
